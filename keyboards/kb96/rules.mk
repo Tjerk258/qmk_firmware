8a37c97f893310c64b53aa27a5a39eb3d8d358e1
Tjerk <tjtd1234@gmail.com>
2024-09-21 08:31:03 +0200

    remove keyboards

--- keyboards/kb96/rules.mk
+++
@@ -1,5 +0,0 @@
-# This file intentionally left blank
-#MCU = STM32F103
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = is31fl3733
-#DEBOUNCE_TYPE = asym_eager_defer_pk
--- keyboards/numpadulp/rules.mk
+++
@@ -1,4 +0,0 @@
-# This file intentionally left blank
-# This file intentionally left blank
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = IS31FL3733
